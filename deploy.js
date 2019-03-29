da47918e39c138ec7b77831e943c49d4692eeb6c
張捷中 <hahababee@gmail.com>
2019-03-29 14:47:27 +0800

    Add git history page

--- deploy.js
+++ deploy.js
@@ -1,3 +1,32 @@
+//Web config===========================================================
+const express = require('express');
+const bodyparser=require("body-parser");
+
+// mysql config
+const mysql=require("mysql2");
+const mysqlCon=mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"Momei#206405",
+  database:"webhook_test"
+});
+mysqlCon.connect(function(err){
+  if(err){
+    throw err;xw
+  }else{
+    console.log("Connected!");
+  }
+});
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+//Web config End===========================================================
+
+
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/whtest', secret: '0930999' }) 
@@ -27,6 +56,39 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
+  console.log(event.payload);
+
   run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
+
+  let commitTime = Date.now();
+  let insertQuery = "INSERT INTO test1(commit_time) VALUES("+commitTime+")";
+  mysqlCon.query(insertQuery, (err, result) => {
+    if (err) throw err;
+    console.log("Insert successfully!")
+  })
 })
+
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use('/', express.static('./'));
+
+app.get('/', (req, res) => {
+  res.redirect('/index.html');
+});
+
+app.get('/record', (req, res) => {
+  let query = "select commit_time from test1";
+  mysqlCon.query(query, (err, result) => {
+    if (err) throw err;
+    let total = {};
+    total.title = result;
+    res.send(total);
+  })
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
