4caad6ea060eecf0c46775b2c06a721f34ca7da6
張捷中 <hahababee@gmail.com>
2019-03-29 13:43:47 +0800

    Modify deploy.sh for better CD process

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ WEB_PATH='/home/ec2-user/Web-Backend-2019-Spring/students/JieChung/stylish'
 echo "Start deployment"
 cd $WEB_PATH
 echo "pulling source code..."
-git pull origin master 
-echo "changing permissions..."
-sudo node index.js
+git pull origin master
+echo "npm install..."
+npm install
+echo "stop pm2..."
+sudo pm2 stop 0
+echo "Restart pm2..."
+sudo pm2 start index.js
 echo "Finished."
